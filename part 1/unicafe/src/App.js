22eb07d563a1f5f15f6fb522ffc4a02f41aabc55
Kevin <HeadbuttStudios@gmail.com>
2021-09-10 19:47:57 -0700

    completed 1.7

--- part 1/unicafe/src/App.js
+++ part 1/unicafe/src/App.js
@@ -7,18 +7,23 @@ const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const [total, setTotal] = useState(0);
 
     const handleGoodClick = () => {
         setGood(good + 1);
+        setTotal(total + 1);
     };
 
     const handleNeutralClick = () => {
         setNeutral(neutral + 1);
+        setTotal(total + 1);
     };
 
     const handleBadClick = () => {
         setBad(bad + 1);
+        setTotal(total + 1);
     };
+
     return (
         <div>
             <h1>give feedback</h1>
@@ -30,6 +35,9 @@ const App = () => {
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {total}</p>
+            <p>average {good - bad / total}</p>
+            <p>positive {(good / total) * 100} %</p>
         </div>
     );
 };
