cc652dc6d69c7e14c233b4ba2c8c7204de25a04a
Kevin <HeadbuttStudios@gmail.com>
2021-10-03 17:43:37 -0700

    2.7 finished

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -3,28 +3,43 @@ import Person from "./components/Person";
 
 const App = (props) => {
     const [persons, setPersons] = useState(props.persons);
-    const [newPerson, setNewPerson] = useState("Enter a new name");
+    const [newName, setNewName] = useState("Enter a new name");
 
     const handleTextChange = (event) => {
-        setNewPerson(event.target.value);
+        setNewName(event.target.value);
     };
 
     const addPerson = (event) => {
         event.preventDefault();
         const personObject = {
-            name: newPerson,
+            name: newName,
         };
-        setPersons(persons.concat(personObject));
-        setNewPerson("Enter another name");
+        const isFound = persons.some((person) => person.name.toLowerCase() === newName.toLowerCase());
+        if (!isFound) {
+            setPersons(persons.concat(personObject));
+            setNewName("Enter another name");
+        } else {
+            alert(`${newName} is already added to phonebook`);
+        }
+        // for (let i = 0; i < persons.length; i++) {
+        //     if (persons[i].name === newName) {
+        //         console.log("duplicate");
+        //         break;
+        //     } else {
+        //         setPersons(persons.concat(personObject));
+        //         setNewName("Enter another name");
+        //         console.log("added");
+        //         break;
+        //     }
+        // }
     };
 
     return (
         <div>
             <h2>Phonebook</h2>
-
             <form onSubmit={addPerson}>
                 <div>
-                    name: <input value={newPerson} onChange={handleTextChange} />
+                    name: <input value={newName} onChange={handleTextChange} />
                 </div>
                 <div>
                     <button type="submit">add</button>
