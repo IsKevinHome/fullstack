f0ff7321b14797af59030cfd96d1e6ae8c650af3
Kevin <HeadbuttStudios@gmail.com>
2021-10-04 21:30:20 -0700

    refactored a bit

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -2,29 +2,52 @@ import React, { useState } from "react";
 import Person from "./components/Person";
 
 const App = (props) => {
-    const [persons, setPersons] = useState(props.persons);
-    const [newName, setNewName] = useState("Enter a new name");
-    const [newNumber, setNewNumber] = useState("Enter a new number");
+    const [contacts, setContacts] = useState([
+        { name: "Guy Fieri", number: "020-4837473" },
+        { name: "Gordon Ramsay", number: "75749483832" },
+        { name: "Mr. Tasty", number: "43-4982839" },
+        { name: "Dude man", number: "11-33-448382" },
+    ]);
+    const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
+    const [showAll, setShowAll] = useState(true);
+    const [filterBy, setFilterBy] = useState("");
 
-    const handleTextChangeName = (event) => {
+    const handleContactNameChange = (event) => {
         setNewName(event.target.value);
     };
 
-    const handleTextChangeNumber = (event) => {
+    const handleContactNumberChange = (event) => {
         setNewNumber(event.target.value);
     };
 
-    const addPerson = (event) => {
+    const handleFiltering = (event) => {
+        console.log(event.target.value);
+        // We get the value, and change it to what we're filtering through.
+        setFilterBy(event.target.value);
+        // Set this false, so that it disables showing all of our contacts
+        setShowAll(false);
+        console.log(filterBy);
+    };
+
+    const addContact = (event) => {
         event.preventDefault();
-        const personObject = {
-            name: newName,
-            number: newNumber,
-        };
-        // Conditional, boolean value
-        const isFound = persons.some((person) => person.name.toLowerCase() === newName.toLowerCase());
-        // that way we can use ! to check if it's true or not.
-        if (!isFound) {
-            setPersons(persons.concat(personObject));
+        //
+        if (newName === "") return true;
+        if (newNumber === "") return true;
+
+        let checkContact = false;
+        contacts.forEach((contact) => {
+            if (contact.name === newName) checkContact = true;
+        });
+
+        if (!checkContact) {
+            const contactObject = {
+                name: newName,
+                number: newNumber,
+            };
+
+            setContacts([...contacts, contactObject]);
             setNewName("");
             setNewNumber("");
         } else {
@@ -32,27 +55,27 @@ const App = (props) => {
         }
     };
 
+    const persons = contacts.map((person) => <Person person={person} />);
+
     return (
         <div>
             <h2>Phonebook</h2>
-            <form onSubmit={addPerson}>
+            Filter:
+            <input value={filterBy} onChange={handleFiltering} />
+            <h2>Add a new</h2>
+            <form onSubmit={addContact}>
                 <div>
-                    name: <input value={newName} onChange={handleTextChangeName} />
+                    name: <input value={newName} onChange={handleContactNameChange} />
                 </div>
                 <div>
-                    number: <input value={newNumber} onChange={handleTextChangeNumber} />
+                    number: <input value={newNumber} onChange={handleContactNumberChange} />
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
-
             <h2>Numbers</h2>
-            <ul>
-                {persons.map((person) => (
-                    <Person person={person} />
-                ))}
-            </ul>
+            <ul>{persons}</ul>
         </div>
     );
 };
