3912e76587a30ef201918aa90f41959a3f9118fd
Kevin <HeadbuttStudios@gmail.com>
2021-10-10 11:39:46 -0700

    refactored into components.

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import Person from "./components/Person";
+import AddContact from "./components/AddContact";
+import SearchContacts from "./components/SearchContacts";
+import RenderContacts from "./components/RenderContacts";
 
 const App = (props) => {
     const [contacts, setContacts] = useState([
@@ -24,6 +28,8 @@ const App = (props) => {
         setFilterBy(event.target.value);
         setShowAll(false);
     };
+
+    //  ALL OF THE ABOVE ARE STATE AND EVENT HANDLERS, THEY STAY INSIDE OUR FUNCTION.
 
     const addContact = (event) => {
         event.preventDefault();
@@ -69,22 +75,9 @@ const App = (props) => {
     return (
         <div>
             <h2>Phonebook</h2>
-            Filter:
-            <input value={filterBy} onChange={handleFiltering} />
-            <h2>Add a new Contact</h2>
-            <form onSubmit={addContact}>
-                <div>
-                    name: <input value={newName} onChange={handleContactNameChange} />
-                </div>
-                <div>
-                    number: <input value={newNumber} onChange={handleContactNumberChange} />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
-            <h2>Contacts</h2>
-            <ul>{rows()}</ul>
+            <SearchContacts filterBy={filterBy} handleFiltering={handleFiltering} />
+            <AddContact addContact={addContact} newName={newName} newNumber={newNumber} handleContactNameChange={handleContactNameChange} handleContactNumberChange={handleContactNumberChange} />
+            <RenderContacts renderContacts={rows()} />
         </div>
     );
 };
