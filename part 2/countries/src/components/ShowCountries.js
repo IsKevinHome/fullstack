0508644c54a19506667420fb2cfb8701f532649a
Kevin <HeadbuttStudios@gmail.com>
2021-10-20 15:28:50 -0700

    2.12 finished

--- part 2/countries/src/components/ShowCountries.js
+++ part 2/countries/src/components/ShowCountries.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Country from "./Country";
+import Countries from "./Countries";
 
 const ShowCountries = ({ filteredCountries, setSearchFilter }) => {
     if (filteredCountries.length > 10) {
         return <div>Too many matches, specify another filter.</div>;
+    } else if (filteredCountries.length === 1) {
+        return filteredCountries.map((country) => {
+            return <Country country={country} />;
+        });
     } else {
         return filteredCountries.map((country) => {
-            return <Country name={country.name.common} />;
+            return <Countries name={country.name.common} />;
         });
     }
 };
