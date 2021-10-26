80f76cfbfaed0e48c38472c7edf44cc53cca8d87
Kevin <HeadbuttStudios@gmail.com>
2021-10-26 15:22:06 -0700

    3.2

---
+++ part 3/phonebook backend/index.js
@@ -0,0 +1,45 @@
+const { response } = require("express");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const contacts = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+    },
+];
+
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/info", (req, res) => {
+    var date = new Date();
+    res.send(`<div>Phonebook has info for ${contacts.length} people</div> <p>${date}</p>`)
+})
+
+app.get("/api/persons", (request, response) => {
+    response.json(contacts);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
